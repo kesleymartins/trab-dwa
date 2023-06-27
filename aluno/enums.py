65fb4c5addf268c9105489a4ec550309689b0cd3
kesleymartins <kesley.martins@vivaldi.net>
2023-06-27 19:00:36 -0300

    use django enums

---
+++ aluno/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Situation(Enum):
+    CURSANDO = "Cursando"
+    APROVADO = "Aprovado"
+    REPROVADO = "Reprovado"
--- aluno/models.py
+++ aluno/models.py
@@ -1,14 +1,14 @@
 from django.db import models
+from .enums import Situation
 
 class Student(models.Model):
-    situations = (
-        (1, 'Cursando'),
-        (2, 'Aprovado'),
-        (3, 'Reprovado'),
-    )
-
     name = models.CharField(max_length=100)
     entryPeriod = models.CharField(max_length=6)
     email = models.EmailField(max_length=100)
     grade = models.FloatField()
-    situation = models.IntegerField(choices=situations, default=1)
+
+    situation = models.CharField(
+        choices=[(tag.value, tag.value) for tag in Situation], 
+        default=Situation.APROVADO, 
+        max_length=100
+    )
