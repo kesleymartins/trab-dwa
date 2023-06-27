f8574126ef31321936326bf12979df1e62a2711b
kesleymartins <kesley.martins@vivaldi.net>
2023-06-27 13:27:51 -0300

    create base student model

--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    situations = (
+        (1, 'Cursando'),
+        (2, 'Aprovado'),
+        (3, 'Reprovado'),
+    )
+
+    name = models.CharField(max_length=100)
+    entryPeriod = models.CharField(max_length=6)
+    email = models.EmailField(max_length=100)
+    frade = models.FloatField()
+    situation = models.IntegerField(choices=situations, default=1)
