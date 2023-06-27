50ab2951647feff9f79f488e9492e2b39098f700
kesleymartins <kesley.martins@vivaldi.net>
2023-06-27 18:46:44 -0300

    add aluno.index as root path

--- trabdwa/urls.py
+++ trabdwa/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('students/', include('aluno.urls')),
+    path('', include('aluno.urls')),
     path('admin/', admin.site.urls),
 ]
